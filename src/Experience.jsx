d44dd817fe7768106bf4402348b7c747eaa09a90
Mohsen <mohsen.norouzi@live.com>
2026-06-26 16:47:31 +0200

    Refactor lighting setup in Experience component; update spotLight properties for improved shadow handling and dynamic controls. Comment out unused ambientLight and previous spotLight configurations for clarity.

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -6,12 +6,22 @@ import { CAMERA, WINDOW_LIGHT } from "./constants/SceneConstants";
 const Experience = () => {
 	// SpotLight Helpers
 	// const spotLightRef = useRef();
-	// const { position, intensity, color, angle } = useControls("Window Light", {
+	// const {
+	// 	position,
+	// 	intensity,
+	// 	color,
+	// 	angle,
+	// 	penumbra,
+	// 	shadowRadius,
+	// 	shadowMapSize,
+	// } = useControls("Window Light", {
 	// 	position: { value: WINDOW_LIGHT.position, min: -50, max: 50, step: 0.1 },
-	// 	intensity: { value: WINDOW_LIGHT.intensity, min: 0, max: 1000, step: 0.1 },
-	// 	color: { value: WINDOW_LIGHT.color },
-	// 	angle: { value: 0.6, min: 0, max: 1, step: 0.01 },
+	// 	intensity: { value: 300, min: 0, max: 1000, step: 0.1 },
+	// 	color: { value: "#fff4e2" },
+	// 	angle: { value: 0.78, min: 0, max: 1, step: 0.01 },
+	// 	penumbra: { value: 0.45, min: 0, max: 1, step: 0.01 },
 	// });
+
 	// useHelper(spotLightRef, SpotLightHelper, "cyan");
 
 	// const { position, rotation } = useControls("Camera", {
@@ -32,9 +42,20 @@ const Experience = () => {
 				fov={45}
 			/>
 
-			<ambientLight intensity={0.5} />
+			{/* <ambientLight intensity={0.4} /> */}
 
 			<spotLight
+				castShadow
+				position={WINDOW_LIGHT.position}
+				intensity={WINDOW_LIGHT.intensity}
+				color={WINDOW_LIGHT.color}
+				penumbra={1}
+				shadow-radius={WINDOW_LIGHT.shadowRadius}
+				shadow-mapSize={WINDOW_LIGHT.shadowMapSize}
+				angle={WINDOW_LIGHT.angle}
+			/>
+
+			{/* <spotLight
 				// ref={spotLightRef}
 				castShadow
 				position={WINDOW_LIGHT.position}
@@ -44,6 +65,19 @@ const Experience = () => {
 				shadow-radius={10}
 				shadow-mapSize={[1024, 1024]}
 				angle={WINDOW_LIGHT.angle}
+			/> */}
+
+			<spotLight
+				// ref={spotLightRef}
+				castShadow
+				position={[9, 19, 22]}
+				intensity={400}
+				color="#ffffff"
+				penumbra={0.45}
+				shadow-radius={3}
+				shadowapSize={[1024, 1024]}
+				angle={0.78}
+				shadowMapSize={[1024, 1024]}
 			/>
 			<KazeModel />
 		</>
